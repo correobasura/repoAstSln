72d041ec7c94c7cef0a28d3b20ccfc584d305d0a
correo basura <correobasuraregistro@gmail.com>
2016-05-20 07:40:49 -0500

    Avance de validación de datos

--- AstSln/Presenter/PAccesoDatos.cs
+++ AstSln/Presenter/PAccesoDatos.cs
@@ -1,4 +1,5 @@
-﻿using IView;
+﻿using Constantes;
+using IView;
 using Model.DataContextModel;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,6 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Constantes;
 
 namespace Presenter
 {
@@ -19,6 +19,20 @@ namespace Presenter
         private List<ASTR> listaDatosSol;
         private List<ASTR> listaDatosLun;
         private List<ASTR> listaDatosGeneral;
+        Dictionary<string, int> dicSign = new Dictionary<string, int>();
+        private const string ACUARIO = "ACUARIO";
+        private const string ARIES = "ARIES";
+        private const string CANCER = "CANCER";
+        private const string CAPRICORNIO = "CAPRICORNIO";
+        private const string ESCORPION = "ESCORPION";
+        private const string GEMINIS = "GEMINIS";
+        private const string LEO = "LEO";
+        private const string LIBRA = "LIBRA";
+        private const string PISCIS = "PISCIS";
+        private const string SAGITARIO = "SAGITARIO";
+        private const string TAURO = "TAURO";
+        private const string VIRGO = "VIRGO";
+
 
         /// <summary>
         /// Constructor de la clase
@@ -28,6 +42,18 @@ namespace Presenter
         {
             this._astEntities = new AstEntities();
             this._iAccesoDatos = iAccesoDatos;
+            this.dicSign.Add(ACUARIO, 0);
+            this.dicSign.Add(ARIES, 0);
+            this.dicSign.Add(CANCER, 0);
+            this.dicSign.Add(CAPRICORNIO, 0);
+            this.dicSign.Add(ESCORPION, 0);
+            this.dicSign.Add(GEMINIS, 0);
+            this.dicSign.Add(LEO, 0);
+            this.dicSign.Add(LIBRA, 0);
+            this.dicSign.Add(PISCIS, 0);
+            this.dicSign.Add(SAGITARIO, 0);
+            this.dicSign.Add(TAURO, 0);
+            this.dicSign.Add(VIRGO, 0);
         }
 
         /// <summary>
@@ -46,6 +72,7 @@ namespace Presenter
             this.ObtenerUltimoResultado();
             this.GetNumerosDespuesDelActual(listaDatosSol, _resultActualSol);
             this.GetNumerosDespuesDelActual(listaDatosLun, _resultActualLun);
+            this.ValidarRachas(listaDatosSol);
         }
 
         /// <summary>
@@ -65,12 +92,15 @@ namespace Presenter
             int[] contadorPosDos = new int[10];
             int[] contadorPosTres = new int[10];
             int[] contadorPosCuatro = new int[10];
-            Dictionary<string, int> dicSign = new Dictionary<string, int>();
             bool flagPosUno = false;
             bool flagPosDos = false;
             bool flagPosTres = false;
             bool flagPosCuatro = false;
             bool flagSign = false;
+            var clonedDictionary = dicSign.ToDictionary(
+                x => x.Key, // Typically no cloning necessary (immuable)
+                x => (int)x.Value  // Do the copy how you want
+            );
             foreach (var item in listaValidar)
             {
                 if (flagPosUno)
@@ -89,14 +119,15 @@ namespace Presenter
                 {
                     contadorPosCuatro[(int)item.POS_CUATRO]++;
                 }
-                //if (flagSign)
-                //{
-                //    dicSign.it
-                //}
+                if (flagSign)
+                {
+                    clonedDictionary[item.SIGN]++;
+                }
                 flagPosUno = this.ValidarMismoDato(1, item, sorComparador);
                 flagPosDos = this.ValidarMismoDato(2, item, sorComparador);
                 flagPosTres = this.ValidarMismoDato(3, item, sorComparador);
                 flagPosCuatro = this.ValidarMismoDato(4, item, sorComparador);
+                flagSign = this.ValidarMismoDato(5, item, sorComparador);
             }
         }
 
@@ -132,6 +163,48 @@ namespace Presenter
                     return false;
             }
             return bFlag;
+        }
+
+        private void ValidarRachas(List<ASTR> listaValidar)
+        {
+            Dictionary<int, List<int>> dicPosUno = this.InicializarDiccionarioRachas();
+            Dictionary < int, List < int >> dicPosDos = this.InicializarDiccionarioRachas();
+            Dictionary < int, List < int >> dicPosTres = this.InicializarDiccionarioRachas();
+            Dictionary < int, List < int >> dicPosCuatro = this.InicializarDiccionarioRachas();
+            //Dictionary < int, List < int >> dicSign = this.InicializarDiccionarioRachas();
+            foreach (var item in listaValidar)
+            {
+                this.AdicionarElementoDiccionarioRachas(dicPosUno, (int)item.POS_UNO);
+            }
+        }
+
+        /// <summary>
+        /// Método que inicializa la estructura que lleva los diccionarios de rachas
+        /// </summary>
+        private Dictionary<int, List<int>> InicializarDiccionarioRachas()
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+            for (int i = 0; i < 10; i++)
+            {
+                List<int> lista = new List<int>();
+                dict.Add(i, lista);
+            }
+            return dict;
+        }
+
+        private void AdicionarElementoDiccionarioRachas(Dictionary<int, List<int>> dict, int dato)
+        {
+            foreach (var item in dict)
+            {
+                if(item.Key.Equals(dato))
+                {
+                    item.Value.Add(1);
+                }
+                else
+                {
+                    item.Value.Add(0);
+                }
+            }
         }
     }
 }
