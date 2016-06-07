9ede694c1fff6073cc5f0a9cf0ad72b4bfe8d952
correo basura <correobasuraregistro@gmail.com>
2016-06-07 07:13:32 -0500

    Avance a puntuación de datos

---
+++ AstSln/DTOs/QueryInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class QueryInfo
+    {
+        public int ClaveNum
+        {
+            get;
+            set;
+        }
+
+        public string ClaveSign
+        {
+            get;
+            set;
+        }
+
+        public int Rank
+        {
+            get;
+            set;
+        }
+    }
+}
